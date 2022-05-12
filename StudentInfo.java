6b505f45b9eefdafa9addb1544131ef05aee48d2
shital2195 <102914489+shital2195@users.noreply.github.com>
2022-05-12 12:40:31 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,15 @@
+package test1;
+
+public class StudentInfo {
+
+	public static void main(String[] args) {
+        student1 obj = new Student1();
+        obj.ss = "Shital";
+        System.out.println(obj.ss);
+        System.out.println(obj.name);
+
+}
+
+	}
+
+
