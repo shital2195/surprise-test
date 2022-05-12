0fe1a211464c07ae7c4f90fc5afa7dba6e24679f
shital2195 <102914489+shital2195@users.noreply.github.com>
2022-05-12 12:48:14 +0530

    piggie bank
    Suppose you have a Piggie Bank with an initial amount of $50 and you have to add some more amount to it.
     Create a class 'AddAmount' with a data member named 'amount' with an initial value of $50. Now make two constructors of this class as follows:
    1 - without any parameter - no amount will be added to the Piggie Bank
    2 - having a parameter which is the amount that will be added to the Piggie Bank
    Create an object of the 'AddAmount' class and display the final amount in the Piggie Bank.

---
+++ addamount.java
@@ -0,0 +1,29 @@
+package pigge.bank;
+
+public class addamount {
+	 private int amount = 50;
+
+	 public addamount()
+
+	 {
+
+	 }
+
+	 public addamount(int a)
+
+	 {
+
+	 amount = a+amount;
+
+	 }
+
+	 public void displayAmount()
+
+	 {
+
+	 System.out.println(amount);
+
+	 }
+}
+
+
