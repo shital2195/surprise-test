11f18e35e7967f568bd244396fddc0e51db1eab2
shital2195 <102914489+shital2195@users.noreply.github.com>
2022-05-12 12:53:46 +0530

    printnumber
    Create a class named 'PrintNumber' to print various numbers of different datatypes by creating 
    different functions with the same name 'printn' having a parameter for each datatype.

---
+++ Printnumber.java
@@ -0,0 +1,48 @@
+package com.datatype;
+
+public class Printnumber {
+	public int print(int a)
+	{
+		
+        System.out.println(a);
+        return a;
+    }
+     public float print(float b)
+     {
+         System.out.println(b);
+        return b;
+    }
+      public double print(double c)
+      {
+           System.out.println(c);
+        return c;
+       
+    }
+       public String print(String d)
+       {
+           System.out.println(d);
+        return d;
+}
+       public static void main(String[] args) {
+    	      
+    	   Printnumber a = new Printnumber ();
+    	   Printnumber b = new Printnumber ();
+    	   a.print("Shital");
+    	   a.print(9.23);
+    	   a.print(0.2221);
+    	   a.print(25);
+
+       }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
