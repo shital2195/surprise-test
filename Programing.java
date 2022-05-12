a263a20d007d213ae073455ef623c9bf6b1e74a4
shital2195 <102914489+shital2195@users.noreply.github.com>
2022-05-12 12:56:22 +0530

    programming
    Create a class named 'Programming'. While creating an object of the class, 
    if nothing is passed to it, then the message "I love programming languages" should be printed. 
    If some String is passed to it, then in place of "programming languages" the name of that String variable should be printed.
    For example, while creating the object if we pass "cpp", then "I love cpp" should be printed.

---
+++ Programing.java
@@ -0,0 +1,16 @@
+package com.program;
+
+public class Programing {
+
+	public static void main(String[] args) {
+		Trial obj = new Trial();
+        obj.ss = "Java";
+        System.out.println("I love " + obj.ss);
+        System.out.println(obj.name);
+
+		
+		
+
+	}
+
+}
